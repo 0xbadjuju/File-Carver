f8995bf8b26834409587dd5a4f714b80dc93accd
Alexander Leary <Alexander@Gegitech.Net>
2013-04-20 16:11:06 -0400

    File list parsing

--- tool.py
+++ tool.py
@@ -26,21 +26,33 @@ def hashfile():
 	digest = (md5.hexdigest(), sha1.hexdigest()) #make immutable
 	return digest;
 
+def file_list():
+	fls = "fls -prlo 32 "+sys.argv[1]
+	file_dictionary = {}
+	file_list = ipc_shell(fls)
+	for single_file in file_list:
+		single_file = single_file.split()
+		if single_file[0] == 'r/r':
+			if single_file[1] == "*":
+				file_dictionary[single_file[3]] = single_file[2]
+			else:
+				file_dictionary[single_file[2]] = single_file[1]
+	return file_dictionary;
+
 def main():
 
 	original_digest = hashfile()
 
 	fdisk 	= "fdisk -l "+sys.argv[1]
-	fls 	= "fls -prlo 32 "+sys.argv[1]
 	icat	= "icat -o 32 "+sys.argv[1]
 	dd	= "if="+sys.argv[1]+" of="
 
 	image_details = ipc_shell(fdisk)
 	print image_details[9]
-	
-	file_list = ipc_shell(fls)
-	file_list_line = file_list[1].split()
-	print file_list_line[2]
+
+	files = file_list()
+	for single_file in files:
+		print single_file, files[single_file]
 
 	#icat = icat + file offset + " < " + output file
 	#carve = ipc_shell(icat)
