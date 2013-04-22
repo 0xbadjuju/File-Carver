a54a602102ca11ff98923cb504db2dacf9bf8f81
Alexander Leary <Alexander@Gegitech.Net>
2013-04-22 16:53:44 -0400

    Menu Framework

--- tool.py
+++ tool.py
@@ -45,7 +45,7 @@ def new_db(db_name):
 	db_info = {"db_connect":"","db_cursor":""}
 	db_info["db_connect"] = sqlite3.connect(db_name+".db")
 	db_info["db_cursor"] = db_info["db_connect"].cursor()
-	db_info["db_cursor"].execute("""CREATE TABLE files(name text, inode text, file_number text)""")
+	db_info["db_cursor"].execute("""CREATE TABLE files(name text, inode int, file_number int)""")
 	return db_info;
 
 def open_db():
@@ -57,16 +57,28 @@ def open_db():
 
 def query_row_db(db_info, db_row):  
     	db_info["db_cursor"].execute("SELECT * FROM files")
-	while True:
-		file = db_info["db_cursor"].fetchone()
-		if file == None:
-           		break
-		print "%3s %-30s %5s" % (file[2], file[0], file[1])
+	print_query_db(db_info)
 	return;
 
-def query_db(db_info, string):
+def query_name_db(db_info, string):
 	db_query = "SELECT * FROM files WHERE name LIKE ?"
 	db_info["db_cursor"].execute(db_query, ('%'+string+'%',))
+	print_query_db(db_info)
+	return;
+
+def query_inode_db(db_info, string):
+	db_query = "SELECT * FROM files WHERE inode LIKE ?"
+	db_info["db_cursor"].execute(db_query, ('%'+string+'%',))
+	print_query_db(db_info)
+	return;
+
+def query_file_number_db(db_info, string):
+	db_query = "SELECT * FROM files WHERE file_number LIKE ?"
+	db_info["db_cursor"].execute(db_query, ('%'+string+'%',))
+	print_query_db(db_info)
+	return;
+
+def print_query_db(db_info):
 	while True:
 		file = db_info["db_cursor"].fetchone()
 		if file == None:
@@ -83,41 +95,68 @@ def insert_file_list(db_info):
 		if single_file[0] == 'r/r':
 			iteration+=1
 			if single_file[1] == "*":
+				single_file[2] = re.sub(":", "",single_file[2])
 				file_info = [(single_file[3], single_file[2], iteration)]
 			else:
+				single_file[1] = re.sub(":", "",single_file[1])
 				file_info = [(single_file[2], single_file[1], iteration)]
 			db_info["db_cursor"].executemany("INSERT INTO files VALUES (?,?,?)", file_info)
 			db_info["db_connect"].commit()
 	return;
 
 def main():
-	fdisk 	= "fdisk -l "+sys.argv[1]
-	#original_digest = hashfile()
-	
+	original_digest = hashfile()
+
 	while True:
-		choice = raw_input("New or Existing Database? ")
-		if re.match('^new$', choice, re.IGNORECASE):
-			db_info = new_db("test")
-			break
-		elif re.match('^old$', choice, re.IGNORECASE):
-			db_info = open_db()
+		choice = raw_input("\
+		1. Populate Database \n\
+		2. Print Database Contents \n\
+		3. Search Database \n\
+		4. Carve File \n\
+		5. Carve Partition \n\
+		6. Exit \n")
+
+		if re.match('^Populate|1$', choice, re.IGNORECASE):
+			insert_file_list(db_info)
+			print "Database Populated"
+			while True:
+				choice = raw_input("New or Existing Database? ")
+				if re.match('^new$', choice, re.IGNORECASE):
+					db_info = new_db("test")
+					break
+				elif re.match('^old$', choice, re.IGNORECASE):
+					db_info = open_db()
+					break
+				else:
+					print "Bad Input"
+		elif re.match('^Print|2$', choice, re.IGNORECASE):
+			query_row_db(db_info, "name")
+		elif re.match('^Search|3$', choice, re.IGNORECASE):
+			while True:
+				type = raw_input("Type of Search? \n 1. By Name \n 2. By Inode \n:")
+				search = raw_input("\nString to search for? ")
+				if re.match('^Name|1$', type, re.IGNORECASE):
+					query_name_db(db_info, search)
+					break
+				elif re.match('^Inode|2$', type, re.IGNORECASE):
+					query_inode_db(db_info, search)
+					break
+				else:
+					print "Bad Input"
+		elif re.match('^File|4$', choice, re.IGNORECASE):
+			carve_file(db_info, string)
+		elif re.match('^Partition|5$', choice, re.IGNORECASE):
+			print ipc_shell("fdisk -l "+sys.argv[1])
+		elif re.match('^Exit|6$', choice, re.IGNORECASE):
 			break
 		else:
 			print "Bad Input"
 
-	image_details = ipc_shell(fdisk)
-
-	insert_file_list(db_info)
-
-	query_row_db(db_info, "name")
-
-	query_db(db_info, "NIKON")
-	
-	#final_digest = hashfile()
-	#if original_digest != final_digest:
-	#	print "\n\n\n Warning File Altered \n\n\n"
-	#else:
-	#	print "File Unaltered"
+	final_digest = hashfile()
+	if original_digest != final_digest:
+		print "\n\n\n Warning File Altered \n\n\n"
+	else:
+		print "File Unaltered"
 	return;
 
 if __name__ == "__main__":
